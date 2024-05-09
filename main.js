0e79dce59867ba8b011f446b3a15956620ae979f
samra <samraabdulhameed7@gmail.com>
2024-05-09 16:59:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+function free(shopName, rent, itemsList) {
+    console.log(`My shop name is ${shopName}...`);
+    console.log(`My shop rent is ${rent}k.`);
+    console.log(`These items are available in my shop:- ${itemsList} etc.`);
+    let wholeSale = "My shop is WholeSale.";
+    return wholeSale;
+}
+let a = free("Almas General Store", 40, " \n floor: \n sugar: \n tealeaves: \n milkpowder: \n rice: \n soap: \n shampoo");
+console.log(a);
+console.log("This is my shop location:");
+let lct = "Saddar near Govonor House!!!";
+console.log(lct);
---
+++ main.ts
@@ -0,0 +1,17 @@
+function free(shopName: string, rent: number, itemsList: string) {
+  console.log(`My shop name is ${shopName}...`);
+  console.log(`My shop rent is ${rent}k.`);
+  console.log(`These items are available in my shop:- ${itemsList} etc.`);
+  let wholeSale = "My shop is WholeSale.";
+  return wholeSale;
+}
+let a = free(
+  "Almas General Store",
+  40,
+  " \n floor: \n sugar: \n tealeaves: \n milkpowder: \n rice: \n soap: \n shampoo"
+);
+console.log(a);
+console.log("This is my shop location:");
+let lct = "Saddar near Govonor House!!!"
+console.log(lct);
+
